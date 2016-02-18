b64d539f53281a52edf9fe1c0c330468b1c0fada
Renan Stigliani <malke.eklam@gmail.com>
2016-02-17 21:42:35 -0500

    Refactor: Orders - changing chained replaces by foreach

--- js/Orders.js
+++ js/Orders.js
@@ -27,19 +27,11 @@
 
                 var orders = [];
 
-                var ordersPerAreaArray = text
-                    .replace(/ cp(\n|$)/ig, ' power-1$1')
-                    .replace(/ cp\*(\n|$)/ig, ' power-2$1')
-                    .replace(/ m-1(\n|$)/ig, ' march-0$1')
-                    .replace(/ m\+0(\n|$)/ig, ' march-1$1')
-                    .replace(/ m(\+1)?\*?(\n|$)/ig, ' march-2$2')
-                    .replace(/ r(aid)?(\n|$)/ig, ' raid-1$2')
-                    .replace(/ r(aid)?\*(\n|$)/ig, ' raid-2$2')
-                    .replace(/ d(efend)?(\+1)?(\n|$)/ig, ' defend-1$3')
-                    .replace(/ d(efend)?(\+2)?\*?(\n|$)/ig, ' defend-2$3')
-                    .replace(/ s(upport)?(\n|$)/ig, ' support-1$2')
-                    .replace(/ s(upport)?(\+1)?\*?(\n|$)/ig, ' support-2$3')
-                    .split('\n')
+                angular.forEach(ordersDict, function (order) {
+                    text = text.replace(order.patternCheck, order.patternReplace);
+                });
+                
+                var ordersPerAreaArray = text.split('\n')
 
                 angular.forEach(ordersPerAreaArray, function (ordersPerArea) {
                     var valueSplitted = ordersPerArea.split(': ');
